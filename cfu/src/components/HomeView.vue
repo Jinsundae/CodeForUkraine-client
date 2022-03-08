4a58a2c70d6d5e70aa5b3b935de6aaa81a07c946
suhwan cheon <soo7652@naver.com>
2022-03-09 02:08:34 +0900

    complete api test

---
+++ cfu/src/components/HomeView.vue
@@ -0,0 +1,20 @@
+<template>
+  <img class="fit-picture"
+     src="../assets/homeimage.jpg"
+     alt="home logo"/>
+
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.fit-picture {
+    width: 90%;
+    height: 10%;
+}
+</style>
